2d9c0182b2352cbdfc53dd5ca2154554d02cf0cb
Midhun T Augustine <96012490+MIDHUNTA30@users.noreply.github.com>
2023-07-22 10:54:17 +0530

    Update code1_2.m

--- code1_2.m
+++ code1_2.m
@@ -10,7 +10,7 @@ for k=1:1:1000
 t(k)=k*T;
 time=[0 T];
 % solve using ode45
-[tt,xx]=ode45(@(t,x) nonautonomous(t,x,uk),time,xk,uk);
+[tt,xx]=ode45(@(t,x) nonautonomous(t,x,uk),time,xk);
 % updating state vector, 
 % which acts as the initial state of next iteration.
 % last row in xx vector is taken as current state vector.
